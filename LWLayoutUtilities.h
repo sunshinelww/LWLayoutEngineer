f333141d41cc8585a2ebd3aa3aae3c5de46c12d7
sunshinelww <liweiwei.sunshine@bytedance.com>
2018-06-28 12:59:56 +0800

    add feature
    Signed-off-by: sunshinelww <liweiwei.sunshine@bytedance.com>

---
+++ LWLayoutUtilities.h
@@ -0,0 +1,12 @@
+//
+//  LWLayoutUtilities.h
+//  LWLayoutEngineer
+//
+//  Created by liweiwei.sunshine on 2018/6/27.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LWLayoutUtilities : NSObject
+
+@end
---
+++ LWLayoutUtilities.mm
@@ -0,0 +1,12 @@
+//
+//  LWLayoutUtilities.m
+//  LWLayoutEngineer
+//
+//  Created by liweiwei.sunshine on 2018/6/27.
+//
+
+#import "LWLayoutUtilities.h"
+
+@implementation LWLayoutUtilities
+
+@end
